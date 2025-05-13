85ad724264dc659cc3e1ae578588387b93ca419d
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-05-13 21:59:03 +0100

    update test sh

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 docker run -it --rm quickforge zsh -i -c "
   echo '\n🔧 Step 1: Initial run';
-  /app/quickforge;
+  /app/quickforge -y;
 
   echo '\n🔧 Step 2: Source shell and run again';
   source /root/.zshrc;
-  /app/quickforge;
+  /app/quickforge -y;
 
   echo '\n✅ E2E complete';
 "
